0df239bed1c7608baf4a253250433647e489dcbd
theopin1 <142269695+theopin1@users.noreply.github.com>
2023-12-11 23:53:08 -0300

    Create borramento.c

---
+++ borramento.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void suavização(struct dados *x, struct dados *y, int *n){
+   int matriz[x->lin * x->col];
+   
+   // fazer a média
+   for (int i = 0; i < x->lin; i++) {
+        for (int j = 0; j < x->col; j++) {
+            int soma = 0;    
+            
+            for (int z = -n/2; z <= n/2; z++){
+                for (int h = -n/2; h <= n/2; h++){
+
+                    int linha = i + z;
+                    int coluna = j + h;
+
+                
+                    if (linha >= 0 && linha < x->lin && coluna >= 0 && coluna < x->col) {
+                        soma += matriz[linha * x->col + coluna];
+                    }
+
+                } 
+            }
+     
+            y->data[i * x->col + j] = soma/ (n * n);
+        }
+       
+    }
+}
